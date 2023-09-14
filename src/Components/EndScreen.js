3a8b882a4709253f504bc7e8f735d8ea4d4cce16
Sachit Dahal <sachitdahal33@gmail.com>
2023-09-14 13:45:28 +0545

    fixed

--- src/Components/EndScreen.js
+++ src/Components/EndScreen.js
@@ -1,12 +1,25 @@
-import React, {useContext} from 'react';
-import { QuizContext } from '../Helpers/Contexts';
+import React, { useContext } from 'react';
+import { QuizContext } from '../Helpers/Contexts'; // Use QuizContext instead of GameStateContext
 import { Questions } from '../Helpers/QuestionsBank';
 
+const EndScreen = () => {
+  const { score, setScore, setGameState, userName } = useContext(QuizContext); // Use QuizContext
 
-function EndScreen() {
+  const restartQuiz = () => {
+    setScore(0);
+    setGameState("menu");
+  };
+
   return (
-    <div>EndScreen Component</div>
+    <div className="EndScreen">
+      <h1>Quiz Finished</h1>
+      <h3>{userName}</h3>
+      <h1>
+        {score} / {Questions.length}
+      </h1>
+      <button onClick={restartQuiz}>Restart Quiz</button>
+    </div>
   );
-}
+};
 
-export default EndScreen; // Default export
+export default EndScreen;
